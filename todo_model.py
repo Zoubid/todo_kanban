12336758ec37cf6b017138a51d889f8ba5f94d3f
zoubid <empressbida@gmail.com>
2017-09-06 12:23:08 +0100

    []: kanban and report for todo-app

---
+++ todo_model.py
@@ -0,0 +1,17 @@
+from openerp import models, fields
+
+class TodoTask(models.Model):
+		_inherit = 'toddo.task'
+
+		priority = fields.Selection(
+		[('0', 'Low'), ('1', 'Normal'), ('2', 'High')],
+		'Priority', default='1')
+
+		kanban_state = fields.Selection(
+		[('normal', 'In Progress'),
+		('blocked', 'Blocked'),
+		('done', 'Ready for next stage')],
+		'Kanban State', default='normal')
+
+		tags = fields.Char('tags')
+		color = fields.Integer('Color Index')
